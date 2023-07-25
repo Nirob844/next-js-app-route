c91c697a261cfda06565196c30fd47311d0b0966
Nirob Hasan <nirob427417@gmail.com>
2023-07-25 17:39:42 +0600

    app route

---
+++ src/app/blog/tech-blog/page.js
@@ -0,0 +1,9 @@
+const TechPage = () => {
+  return (
+    <div>
+      <h1>tech page</h1>
+    </div>
+  );
+};
+
+export default TechPage;
---
+++ src/app/news/page.js
@@ -0,0 +1,9 @@
+const NewsPage = () => {
+  return (
+    <div>
+      <h1>news page</h1>
+    </div>
+  );
+};
+
+export default NewsPage;
